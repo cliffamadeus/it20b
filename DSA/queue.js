a07b86419f35e38bc0a1a8745e15753fe9cff9d6
cliffamadeus <dev.clfm5t12@gmail.com>
2025-12-02 07:58:17 +0800

    Added utility methods for DSA/queue.js

--- DSA/queue.js
+++ DSA/queue.js
@@ -20,11 +20,29 @@ class Queue{
         return output;
     }
 
+    is_empty(){
+        return this.frontIndex > this.rearIndex;
+    }
+
+    front(){
+        //Correction Statement
+        if(this.is_empty()) return null;
+        return this.data[this.frontIndex];
+    }
+
+    size(){
+        return this.rearIndex - this.frontIndex +1;
+    }
 }
 
 //
 let queue1 = new Queue();
+console.log(queue1.is_empty());
 queue1.enqueue(4);
+console.log("The current queue size is: " + queue1.size());
 queue1.enqueue(2);
+console.log("The current queue size is: " + queue1.size());
 queue1.enqueue(3);
-console.log(queue1.traverse());
+console.log("The current queue size is: " + queue1.size());
+console.log(queue1.is_empty());
+console.log(queue1.front());
